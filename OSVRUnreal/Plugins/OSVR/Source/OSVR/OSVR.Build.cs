7a3f56c2b95c372e3a003cbff5da157cf10ea618
Ryan Pavlik <ryan@sensics.com>
2015-07-01 15:44:14 -0500

    Clean up build file.

--- OSVRUnreal/Plugins/OSVR/Source/OSVR/OSVR.Build.cs
+++ OSVRUnreal/Plugins/OSVR/Source/OSVR/OSVR.Build.cs
@@ -4,12 +4,6 @@ public class OSVR : ModuleRules
 {
 	public OSVR(TargetInfo Target)
 	{
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				"OSVR/Private",
@@ -17,26 +11,6 @@ public class OSVR : ModuleRules
 				// ... add other private include paths required here ...
 			}
 			);
-
-		// These are UE modules
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				"CoreUObject",      // Provides Actors and Structs
-				"Engine",           // Used by Actor
-				"Slate",            // Used by InputDevice to fire bespoke FKey events
-				"InputCore",        // Provides LOCTEXT and other Input features
-				"InputDevice",      // Provides IInputInterface
-				"RHI",
-				"RenderCore",
-				"Renderer",
-				"ShaderCore",
-				"HeadMountedDisplay",
-				"Json"
-				// ... add other public dependencies that you statically link with here ...
-			}
-		);
 
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
@@ -55,13 +29,6 @@ public class OSVR : ModuleRules
 				"HeadMountedDisplay",
 				"Json"
 				// ... add private dependencies that you statically link with here ...
-			}
-			);
-
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
 			}
 			);
 	}
