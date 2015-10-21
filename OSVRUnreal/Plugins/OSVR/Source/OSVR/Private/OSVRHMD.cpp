ff3d899c7f3fa160a1da07b3451aa03bbaef81cf
JeroMiya <bell.jeremy@gmail.com>
2015-10-21 17:00:21 -0400

    Disabled mouselook pitch/roll. Still allows horizontal mouse movement, but head up and down from the HMD is always respected.

--- OSVRUnreal/Plugins/OSVR/Source/OSVR/Private/OSVRHMD.cpp
+++ OSVRUnreal/Plugins/OSVR/Source/OSVR/Private/OSVRHMD.cpp
@@ -134,8 +134,8 @@ void FOSVRHMD::ApplyHmdRotation(APlayerController* PC, FRotator& ViewRotation)
 
 	// Pitch from other sources is never good, because there is an absolute up and down that must be respected to avoid motion sickness.
 	// Same with roll.
-	//DeltaControlRotation.Pitch = 0;
-	//DeltaControlRotation.Roll = 0;
+	DeltaControlRotation.Pitch = 0;
+	DeltaControlRotation.Roll = 0;
 	DeltaControlOrientation = DeltaControlRotation.Quaternion();
 
 	ViewRotation = FRotator(DeltaControlOrientation * CurHmdOrientation);
